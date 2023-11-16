187583731bdbb7a5d609a20f6d71db018c3eeb4e
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 13:39:28 -0500

    fix: import modules

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QBX-Prison'
+description 'QBX_Prison'
+repository 'https://github.com/Qbox-project/qbx_prison'
 version '1.0.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
-    '@qbx_core/import.lua',
+    '@qbx_core/modules/utils.lua',
     '@qbx_core/shared/locale.lua',
     'locales/en.lua',
     'locales/*.lua',
@@ -14,6 +15,7 @@ shared_scripts {
 }
 
 client_scripts {
+    '@qbx_core/modules/playerdata.lua',
     'client/main.lua',
     'client/jobs.lua',
     'client/prisonbreak.lua'
@@ -21,10 +23,5 @@ client_scripts {
 
 server_script 'server/main.lua'
 
-modules {
-    'qbx_core:playerdata',
-    'qbx_core:utils',
-}
-
-use_experimental_fxv2_oal 'yes'
 lua54 'yes'
+use_experimental_fxv2_oal 'yes'
